4925368138aa315082efde703584a0799b5176fe
Bryan <sealandsky@qq.com>
2025-01-16 09:21:28 +0800

    Add deployment configuration and README

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
-  base: './',
+  base: '/hanzi-stroke-order/',
   plugins: [vue()],
   optimizeDeps: {
     include: ['hanzi-writer', 'hanzi-writer-data/*']
